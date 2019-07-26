2448f22a2afe56ec6d8496b7ea2ad839de6e4f70
aliciaTED <48830896+aliciaTED@users.noreply.github.com>
2019-07-26 11:52:50 +0200

    Revert "new Code"
    This reverts commit 6d66949e13b350291bf4e6282286cfaf0eb97203.

--- Kartenspiel/js/Kartenspiel.ts
+++ Kartenspiel/js/Kartenspiel.ts
@@ -11,6 +11,7 @@ let drawPile : Card [] = []; // Ziehstapel
 let discardPile : Card [] = []; // Ablagestapel
 let computerHand : Card [] = []; // Hand des Computers
 let playerHand : Card [] = []; // Hand des Spielers
+//let cardNumber : number [] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 window.onload = function () {
     document.getElementById("start").addEventListener("click", startGame, false);
@@ -75,9 +76,9 @@ function dealCards (){
         drawPile.splice(0,1);
         playerHand.push(drawPile[0])
         drawPile.splice(0,1);
-        // push fügt Karten dem "Handarray" hinzu und splice löscht ausgeteilte Karten aus dem Kartenstapel (drawPile); i bestimmt Anzahl der Karten --> 7 Karten pro Hand
+        // push fügt Karten dem "Handarray" hinzu und splice löscht ausgeteilte Karten aus dem Kartenstapel (drawPile)
     }
-    // eine Karte soll auf dem Ablagestapel angezeigt werden, damit das Spiel begonnen werden kann
+    //eine Karte soll auf dem Ablagestapel angezeigt werden, damit das Spiel begonnen werden kann
     discardPile.push(drawPile[0]);
     drawPile.splice(0,1);
 
@@ -89,83 +90,96 @@ function dealCards (){
     console.log("Alle Karten wurden verteilt.")
 }
 
-function updateHTML () { // umfasst Erstellung und Leerung der HTML-Elemente, muss bei jeder Änderung des HTML durch Funktionen aufgerufen werden
-    clearHTML();
+function updateHTML () { // umfasst Erstellung und Leerung der HTML-Elemente
+    clearHTML("player");
+    clearHTML("computer");
+    clearHTML("discard");
+    clearHTML("draw");
     generateHTML();
 }
 
 function generateHTML () {
-    for (let i = 0; i < playerHand.length; i++) { // mehrere Handkarten erstellen
+    for (let i = 0; i < playerHand.length; i++) {
         generatePlayerHand (i)
     }
-    
-    for (let i = 0; i < computerHand.length; i++) { // mehrere Computerkarten erstellen
+    for (let i = 0; i < computerHand.length; i++) {
         generateComputerHand (i)
     }
-    generateDiscardPile (); // Ablagestapel soll erstellt werden
-    generateDrawPile(); // Kartenstapel zum Ziehen soll erstellt werden
-
+    for (let i = 0; i < discardPile.length; i++) {
+        generateDiscardPile (i)
+    }
 }
 // Funktionen zum Erzeugen der HTML-Elemente (d.h. <div>, die mit CSS zu Karten gestyled wurden; Verwendung von AppendChild hilfreich, Aufruf durch updateHTML()
 function generatePlayerHand (numberOfCard : number){
-    let holdingDivPlayer : HTMLElement = document.createElement("div"); // div mithilfe von CSS gestyled stellt jede Karte dar
-    holdingDivPlayer.setAttribute("id", "player" + (numberOfCard + 1)) // Zugriff auf Player-Section, in der div erstellt werden soll
-    holdingDivPlayer.setAttribute("class", playerHand[numberOfCard].cardColor); // Zugriff auf Farbklasse inkl. generelles Styling einer Karte
-    holdingDivPlayer.addEventListener("click", function () { playCard(numberOfCard); }, false); // Funktion playCard() wird aufgerufen, Parameter (Kartenzahl) kann eingesetzt werden
-    document.getElementById("player").appendChild(holdingDivPlayer); // erstellte Karte wird zur Player-Section hinzugefügt (Zugriff über ID)
-    
+    let holdingDivPlayer : HTMLElement = document.createElement("div");
+    holdingDivPlayer.setAttribute("id", "player" + (numberOfCard + 1))
+    holdingDivPlayer.setAttribute("class", "card" + playerHand[numberOfCard].cardColor);
+    holdingDivPlayer.addEventListener("click", playCard, false);
+    document.getElementById("player").appendChild(holdingDivPlayer);
+
     // Festlegung der Wertigkeit/Zahl der Karte
-    let newCardNumber : HTMLElement = document.createElement("p"); // Zahl wird innerhalb eines <p> erstellt
-    newCardNumber.innerHTML = playerHand[numberOfCard].cardNumber + ""; // Zugriff auf Wertigkeit bzw. Zahl, die in HTML eingefügt werden soll
-    newCardNumber.setAttribute("class", "cardNumber"); // Zugriff auf Klasse bzw. Styling der Zahl
-    holdingDivPlayer.appendChild(newCardNumber); // Zahl wird zur Player-Section hinzugefügt
+    let newCardNumber : HTMLElement = document.createElement("p");
+    newCardNumber.innerHTML = playerHand[numberOfCard].cardNumber + ""; 
+    newCardNumber.setAttribute("class", "cardNumber");
+    holdingDivPlayer.appendChild(newCardNumber);
 }
 
 function generateComputerHand (numberOfCard : number) {
     let holdingDivComputer : HTMLElement = document.createElement("div");
     holdingDivComputer.setAttribute("id", "computer" + (numberOfCard + 1));
-    holdingDivComputer.setAttribute("class", computerHand[numberOfCard].cardColor); // soll verdeckt angezeigt werden --> hiddenCard
+    holdingDivComputer.setAttribute("class", "card" + "hiddenCard"); // soll verdeckt angezeigt werden
     document.getElementById("computer").appendChild(holdingDivComputer);
-
-    // Überprüfen der Computer-Hand, bis Code vollständig
-    let newCardNumber : HTMLElement = document.createElement("p");
-    newCardNumber.innerHTML = playerHand[numberOfCard].cardNumber + ""; 
-    newCardNumber.setAttribute("class", "cardNumber");
-    holdingDivComputer.appendChild(newCardNumber);
 }
 
-function generateDrawPile () {
+function generateDrawPile (numberOfCard:number) {
     let holdingDivDraw : HTMLElement = document.createElement("div");
-    holdingDivDraw.setAttribute("id", "draw");
-    holdingDivDraw.setAttribute("class", "hiddenCard");
-    holdingDivDraw.addEventListener("click", drawCard, false)
-    document.getElementById("drawPile").appendChild(holdingDivDraw); // Karte soll genau auf gepunkteter Fläche erscheinen, daher Zugriff auf #drawPile
+    holdingDivDraw.setAttribute("id", "draw" + (numberOfCard + 1));
+    holdingDivDraw.setAttribute("class", "card" + "hiddenCard");
+    holdingDivDraw.addEventListener("click", drawCard(numberOfCard));
+    document.getElementById("draw").appendChild(holdingDivDraw);
 
 }
 
-function generateDiscardPile () {
+function generateDiscardPile (numberOfCard:number) {
     let holdingDivDiscard : HTMLElement = document.createElement("div");
-    holdingDivDiscard.setAttribute("id", "discard")
-    holdingDivDiscard.setAttribute("class", discardPile[discardPile.length-1].cardColor);
-    document.getElementById("discardPile").appendChild(holdingDivDiscard); // Karte soll genau auf gepunkteter Fläche erscheinen --> #discardPile
+    holdingDivDiscard.setAttribute("id", "discard" +(numberOfCard + 1));
+    holdingDivDiscard.setAttribute("class", "card");
+    document.getElementById("discard").appendChild(holdingDivDiscard);
 
     // Festlegung der Wertigkeit/Zahl der Karte
     let newCardNumber : HTMLElement = document.createElement("p");
-    newCardNumber.innerHTML = drawPile[discardPile.length-1].cardNumber + "";
+    newCardNumber.innerHTML = drawPile[numberOfCard].cardNumber + "";
     newCardNumber.setAttribute("class", "cardNumber");
     holdingDivDiscard.appendChild(newCardNumber);
 }
-  
-// fehlende Funktionen: drawCard für Kartenstapel; playCards (von Spieler & Computer), clearHTML (um Spiel mit Button neu zu starten), Funktion mit Alert, dass man gewonnen bzw. verloren hat (und dann einfach Neustart???)
 
-function playCard(cardNumber:number) {
+// Funktion, um HTML-Elemente zu löschen (nötig für Neustart eines Spiels)
+function clearHTML(cardClass : string) {
+    let toBeCleared = document.getElementById(cardClass);
+    while(toBeCleared.firstChild){
+        toBeCleared.removeChild(toBeCleared.firstChild)
+    }
+}
+
+function playCard() {
+    // auswählen, ob Spieler oder Computer an der Reihe ist
+    // für Spieler: ausgewählte Karte mit Ablagestapel vergleichen
+    // wenn gespielt werden kann --> Zug zu Ende, evtl. gewonnen
+    // wenn nicht gespielt werden kann --> Karte aufnehmen, Zug zu Ende
+    // Computer spielt, gleiches Prinzip
 
 }
 
-function drawCard(){}
+function drawCard(){
 
+}
+// fehlende Funktionen: drawCard für Kartenstapel; playCards (von Spieler & Computer), clearHTML (um Spiel mit Button neu zu starten), Funktion mit Alert, dass man gewonnen bzw. verloren hat (und dann einfach Neustart???)
 
-function gameWon () { // Spiel soll bei Gewinn von Spieler oder Computer wieder von vorne anfangen
+/*function playCard(array : Card) {
+    if () {
 
-  }
-function clearHTML(){}
+    }
+}
+  function gameWon () { // Spiel soll bei Gewinn von Spieler oder Computer wieder von vorne anfangen
+
+  }*/
